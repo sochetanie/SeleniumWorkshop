ff2e653aea3206ce3e1c91c746d0316f6e7e00c3
sochetanie <allagorik2901@gmail.com>
2019-11-09 15:30:17 -0500

    added 3 tests to test login

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -4,13 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+
 import java.util.concurrent.TimeUnit;
 
 public class DemoTest {
 
   @Test
   public void incorrectPassAndEmail() {
-//    Test Case logic
     WebDriver driver = new ChromeDriver();
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     driver.get("https://deens-master.now.sh/login");
@@ -23,9 +23,39 @@ public class DemoTest {
     WebElement errorMessage = driver.findElement(By.cssSelector(".ui.error.message p"));
     String errorMessageText = errorMessage.getText();
 
-//    Assertion
     Assert.assertEquals(errorMessageText, "Wrong email or password.");
     driver.quit();
+  }
+
+  @Test
+  public void loginEmptyParametrsTest() {
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://deens-master.now.sh/login");
+
+    WebElement loginButton = driver.findElement(By.cssSelector("[data-testid='loginSubmit']"));
+    loginButton.click();
+    WebElement errorMessage = driver.findElement(By.cssSelector(".ui.error.message p"));
+    String errorMessageText = errorMessage.getText();
+
+    Assert.assertEquals(errorMessageText, "Empty email or password");
+    driver.quit();
+  }
+
+  @Test
+  public void loginSuccessTest() throws InterruptedException {
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://deens-master.now.sh/login");
+    driver.findElement(By.cssSelector("#email")).click();
+    driver.findElement(By.cssSelector("#email")).sendKeys("valery.kells0202@gmail.com");
+    driver.findElement(By.cssSelector("#password")).sendKeys("Testing12345");
+    driver.findElement(By.cssSelector("[data-testid='loginSubmit']")).click();
+
+    Thread.sleep(4000);
+    driver.navigate().refresh();
+
+//    Assertion
+    Assert.assertTrue(driver.findElement(By.cssSelector("[class*='DesktopDropDownMenu_AvatarWrapper']")).isDisplayed());
+
   }
 
 }
