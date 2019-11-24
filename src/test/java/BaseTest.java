5a9435cd95f51373d6346353a6adf6a4cecb1549
sochetanie <allagorik2901@gmail.com>
2019-11-24 17:49:08 -0500

    refuctored login and signUp tests

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -7,13 +8,14 @@ import java.util.concurrent.TimeUnit;
 
 
 public class BaseTest {
-  static WebDriver driver;
+  WebDriver driver;
+  WebDriverWait driverWait;
 
   @BeforeMethod
   public void setUp() {
     driver = new ChromeDriver();
-    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     driver.manage().window().maximize();
+    driverWait = new WebDriverWait(driver, 5);
   }
 
   @AfterMethod
--- src/test/java/Login_SignUp_Test.java
+++ src/test/java/Login_SignUp_Test.java
@@ -1,76 +1,81 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-public class DemoTest extends BaseTest {
+public class Login_SignUp_Test extends BaseTest {
 
   private static By email = By.cssSelector("#email");
   private static By pass = By.cssSelector("#password");
   private static By username = By.cssSelector("#username");
-  private static By loginButton = By.cssSelector("[data-testid='loginSubmit']");
-  private static By errorMessage = By.cssSelector(".ui.error.message p");
+
+  private void clickLoginButton() {
+    WebElement loginButton = driverWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[data-testid='loginSubmit']")));
+    loginButton.click();
+  }
+
+  private String getErrorMessage() {
+    WebElement errorMessage = findElementOnThePage(".ui.error.message p");
+    return errorMessage.getText();
+  }
+
+  private WebElement findElementOnThePage(String cssLocator) {
+    return driver.findElement(By.cssSelector(cssLocator));
+  }
+
 
   @Test
   public void incorrectPassAndEmailTest() {
     driver.get("https://deens-master.now.sh");
-    driver.findElement(By.cssSelector("a[href*='login']")).click();
-    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    findElementOnThePage("a[href*='login']").click();
+    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
     driver.findElement(email).sendKeys("valery@gmail.com");
     driver.findElement(pass).sendKeys("Testing123");
-    driver.findElement(loginButton).click();
+    clickLoginButton();
 
-//    Assertion
-    Assert.assertEquals(driver.findElement(errorMessage).getText(), "Wrong email or password.");
+    Assert.assertEquals(getErrorMessage(), "Wrong email or password.");
   }
 
   @Test
   public void loginEmptyParametersTest() {
     driver.get("https://deens-master.now.sh/login");
-    driver.findElement(loginButton).click();
-
-//    Assertion
-    Assert.assertEquals(driver.findElement(errorMessage).getText(), "Empty email or password");
+    clickLoginButton();
+    Assert.assertEquals(getErrorMessage(), "Empty email or password");
   }
 
   @Test
-  public void loginSuccessTest() throws InterruptedException {
+  public void loginSuccessTest() {
     driver.get("https://deens.com/login");
-
     driver.findElement(email).sendKeys("valery.kells0202@gmail.com");
     driver.findElement(pass).sendKeys("Testing12345");
+    findElementOnThePage(".ui.large.fluid.button.green-btn.pl-btn").click();
 
-    driver.findElement(By.cssSelector(".ui.large.fluid.button.green-btn.pl-btn")).click();
+    WebElement userAvatar = driverWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[class*='DesktopDropDownMenu__AvatarWrapper']")));
 
-    Thread.sleep(5000);
-    driver.navigate().refresh();
-
-//    Assertion
-    Assert.assertTrue(driver.findElement(By.cssSelector("[class*='DesktopDropDownMenu__AvatarWrapper']")).isDisplayed());
+    Assert.assertTrue(userAvatar.isDisplayed());
   }
 
   @Test
-  public void signUpSuccessTest() throws InterruptedException {
+  public void signUpSuccessTest() {
+//    driver.get("https://deens-master.now.sh/register");
     driver.get("https://deens.com/register");
 
     Random randomGenerator = new Random();
-    int randomInt = randomGenerator.nextInt(1000);
+    int randomInt = randomGenerator.nextInt(3000);
 
     driver.findElement(username).sendKeys("username" + randomInt);
     driver.findElement(email).sendKeys("username" + randomInt + "@gmail.com");
     driver.findElement(pass).sendKeys("Testing12345");
-    driver.findElement(By.cssSelector(".ui.large.fluid.button.green-btn.pl-btn")).click();
+    findElementOnThePage(".ui.large.fluid.button.green-btn.pl-btn").click();
 
-    Thread.sleep(6000);
-    WebElement imageDiv = driver.findElement(By.cssSelector("[class*='DesktopDropDownMenu__AvatarWrapper']"));
+    WebElement userAvatar = driverWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[class*='DesktopDropDownMenu__AvatarWrapper']")));
+    String img_text = userAvatar.findElement(By.tagName("img")).getAttribute("alt");
 
-    String img_text = imageDiv.findElement(By.tagName("img")).getAttribute("alt");
-
-//    Assertion
     Assert.assertEquals(img_text, "user avatar");
   }
 
@@ -88,7 +93,6 @@ public class DemoTest extends BaseTest {
     driver.findElement(By.cssSelector(".ui.large.fluid.button.green-btn.pl-btn")).click();
     String errorMessageText = driver.findElement(By.cssSelector(".ui.message")).getText();
 
-//    Assertion
     Assert.assertEquals(errorMessageText, "Please enter a valid email address");
   }
 
@@ -96,10 +100,9 @@ public class DemoTest extends BaseTest {
   public void signUpEmptyCredentialsTest() {
     driver.get("https://deens-master.now.sh/register");
 
-    driver.findElement(By.cssSelector(".ui.large.fluid.button.green-btn.pl-btn")).click();
-    String errorMessageText = driver.findElement(By.cssSelector(".ui.message")).getText();
+    findElementOnThePage(".ui.large.fluid.button.green-btn.pl-btn").click();
+    String errorMessageText = findElementOnThePage(".ui.message").getText();
 
-//    Assertion
     Assert.assertEquals(errorMessageText, "Password must be at least 8 characters long");
   }
 
